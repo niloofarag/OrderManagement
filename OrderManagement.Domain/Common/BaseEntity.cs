da42476c528b0073cce916b82475ef2d34496cd8
Niloofar <niloofaraghaei78@gmail.com>
2026-06-30 09:45:00 +0330

    feat(domain): add base entity and domain exception primitives

---
+++ OrderManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace OrderManagement.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; }
+}
---
+++ OrderManagement.Domain/Common/DomainException.cs
@@ -0,0 +1,8 @@
+namespace OrderManagement.Domain.Common;
+
+public abstract class DomainException : Exception
+{
+    protected DomainException(string message) : base(message)
+    {
+    }
+}
